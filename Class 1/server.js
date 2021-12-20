ffaa45a6fba8c5a1fc95d2c8cfe01e167feb9c23
Faryad Az Zim <73974353+faryadazim@users.noreply.github.com>
2021-12-20 06:37:51 +0500

    upd

---
+++ Class 1/server.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+var express = require("express");
+var cors = require("cors");
+var bodyParser = require("body-parser") 
+const port = process.env.PORT || 5000;
+var app = express();
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static('public')) 
+
+
+
+
+var imageRoute = require('./src/imageProject/imageRoutes')
+var adminRoute = require('./src/imageProject/imageRoutes')
+
+app.get("/", function (req, res) {
+  res.send("Server is working");
+});
+
+app.use("/imageProject",imageRoute)
+app.use("/admin",adminRoute)
+
+
+// server port listener
+app.listen(port, (err) => {
+  if (err) {
+    console.log("something went wrong", error);
+  } else {
+    console.log(`server is working on port, ${port}`);
+  }
+});
