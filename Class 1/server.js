2489203e47feea23b9aee80dbc31978808fc3075
Faryad Az Zim <73974353+faryadazim@users.noreply.github.com>
2021-12-22 08:35:07 +0500

    update

--- Class 1/server.js
+++ Class 1/server.js
@@ -3,15 +3,17 @@ var express = require("express");
 var cors = require("cors");
 var bodyParser = require("body-parser") 
 const port = process.env.PORT || 5000;
+
+
 var app = express();
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-app.use(bodyParser.json());
-app.use(cors());
-app.use(express.static('public')) 
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: true,
+//   })
+// );
+// app.use(bodyParser.json());
+// app.use(cors());
+// app.use(express.static('public')) 
 
 
 
---
+++ Practice 1/src/User/routes.js
@@ -0,0 +1,8 @@
+const express =require('express')
+const cors = require('cors')
+const router = express.Router()
+
+
+router.get('/', (req,res)=>{
+    console.log(Amb);})
+module.exports = router;
