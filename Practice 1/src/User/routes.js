febb1ee508ef38f1200b514fc080b9aeb8a712b0
Faryad Az Zim <73974353+faryadazim@users.noreply.github.com>
2021-12-22 08:48:34 +0500

    Update routes.js

--- Practice 1/src/User/routes.js
+++ Practice 1/src/User/routes.js
@@ -1,8 +1,21 @@
-const express =require('express')
-const cors = require('cors')
-const router = express.Router()
+// const express =require('express') 
+// const router = express.Router()
 
 
-router.get('/', (req,res)=>{
-    console.log(Amb);})
+// router.get('/', (req,res)=>{
+//     console.log("Amb");
+// res.send("Le Amb")})
+// module.exports = router;
+
+
+var express = require("express")
+var router = express.Router();
+
+
+ 
+
+router.get('/', (req,res)=>{   
+    console.log("workinf add Data")
+    res.send("workinf add Data")
+}) 
 module.exports = router;
