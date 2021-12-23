3d008f58d0a24ab328f710374991a38a6d5215ba
Faryad Az Zim <73974353+faryadazim@users.noreply.github.com>
2021-12-23 18:54:35 +0500

    update server

--- Practice 1/server.js
+++ Practice 1/server.js
@@ -1,30 +1,22 @@
-const express =require('express')
-const app = express()
-const cors = require('cors')
-var bodyParser = require("body-parser") 
+const express = require("express");
+const app = express();
+const cors = require("cors");
+var bodyParser = require("body-parser");
 const port = process.env.port || 4000;
 
-app.get('/' , (req,res)=>{ 
-res.send("Abe chal jaa bsdk");
-});
-
-
-
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+// MiddleWare
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static('public')) 
+app.use(express.static("public"));
 
+// Rutes
+app.get("/", (req, res) => {
+  res.send("Main server running");
+});
 
-
-var routes = require("./src/User/routes")
-app.use ('/routes',routes)
-
- 
+var routes = require("./src/User/routes");
+app.use("/routes", routes);
 
 app.listen(port, (err) => {
   if (err) {
--- Practice 1/src/User/routes.js
+++ Practice 1/src/User/routes.js
@@ -1,8 +1,9 @@
-const express =require('express') 
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
+router.get("/", (req, res) => {
+  console.log("Amb");
+  res.send("Le Amb");
+});
 
-router.get('/', (req,res)=>{
-    console.log("Amb");
-res.send("Le Amb")})
 module.exports = router;
